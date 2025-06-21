7338eafb1c012bfbbc0388468e06df397b2903ee
Franco Busch <francobusch130@gmail.com>
2025-06-21 03:40:59 -0300

    Cleaning code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TasksModule } from './SimuMDG/simuMDG.module';
-import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [TasksModule, ProjectsModule],
+  imports: [TasksModule],
   controllers: [AppController],
 })
 export class AppModule {}
